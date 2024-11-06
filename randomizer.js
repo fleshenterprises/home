924261e12c8eb96f798c497c7fcc1cb2e28f8464
fleshenterprises <memelousdatabase@gmail.com>
2024-11-05 22:07:29 -0600

    Add files via upload

--- randomizer.js
+++ randomizer.js
@@ -1,17 +1,15 @@
 function firstrandom(number)
 {
-	var andomize = ((Math.floor(Math.random() * 6)) + 1);
+	var andomize = ((Math.floor(Math.random() * 8)) + 1);
 	
 	while (andomize == number)
 	{
-		andomize = ((Math.floor(Math.random() * 6)) + 1);
+		andomize = ((Math.floor(Math.random() * 8)) + 1);
 	}
-
-	andomize = andomize + ".html";
-
-	if (andomize != "1.html") 
+	
+	if (andomize != 1) 
 	{
-		document.getElementById("reseter").href = andomize;
+		document.getElementById("reseter").href = andomize + ".html";
 	}
 
 	else
