b0bce900f291565c7b3e94a0b2f7646a58454b51
fleshenterprises <memelousdatabase@gmail.com>
2025-03-09 01:42:13 -0600

    Add files via upload

--- randomizer.js
+++ randomizer.js
@@ -1,24 +1,84 @@
+var pageArray;
+var checkable;
+
 function firstrandom(number)
 {
 	var andomize = ((Math.floor(Math.random() * 8)) + 1);
-	
-	while (andomize == number)
+	checkable = false;
+
+	if(sessionStorage.getItem("lastTwoPages") == null)
 	{
-		andomize = ((Math.floor(Math.random() * 8)) + 1);
+		pageArray = new Array();
+	}
+	else
+	{
+		pageArray = JSON.parse(sessionStorage.getItem("lastTwoPages"));
 	}
 
-	if((number == 8) || (number ==  7))
+	if(pageArray.length === 0)
 	{
-		while((andomize == 8) || (andomize == 7))
+		pageArray[0] = number;
+	}
+	else if(pageArray.length === 1)
+	{
+		if(number != pageArray[0])
+		{
+			pageArray[1] = number;
+		}
+	}
+	else if(pageArray.length === 2)
+	{
+		if(number != pageArray[1])
+		{
+			pageArray[0] = pageArray[1];
+			pageArray[1] = number;
+		}
+		checkable = true;
+	}
+
+	if(checkable == true)
+	{
+		secondLastPage = pageArray[0];
+		while ((andomize == number) || (andomize == secondLastPage))
 		{
 			andomize = ((Math.floor(Math.random() * 8)) + 1);
 		}
+
+		if((number == 8) || (number ==  7))
+		{
+			while((andomize == 8) || (andomize == 7) || (andomize == secondLastPage))
+			{
+				andomize = ((Math.floor(Math.random() * 8)) + 1);
+			}
+		}
+		else if((number == 1) || (number ==  2))
+		{
+			while((andomize == 1) || (andomize == 2) || (andomize == secondLastPage))
+			{
+				andomize = ((Math.floor(Math.random() * 8)) + 1);
+			}
+		}
 	}
-	else if((number == 1) || (number ==  2))
+	else
 	{
-		while((andomize == 1) || (andomize == 2))
+		while ((andomize == number))
 		{
 			andomize = ((Math.floor(Math.random() * 8)) + 1);
+		}
+
+		if((number == 8) || (number ==  7))
+		{
+			while((andomize == 8) || (andomize == 7))
+			{
+				andomize = ((Math.floor(Math.random() * 8)) + 1);
+			}
+		}
+		else if((number == 1) || (number ==  2))
+		{
+			while((andomize == 1) || (andomize == 2))
+			{
+				andomize = ((Math.floor(Math.random() * 8)) + 1);
+			}
 		}
 	}
 
@@ -30,4 +90,6 @@ function firstrandom(number)
 	{
 		document.getElementById("reseter").href = "index.html";
 	}
+
+	sessionStorage.setItem("lastTwoPages", JSON.stringify(pageArray));
 }
